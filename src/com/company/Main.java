d366e77a7105c57e8e0187be773eed90bd61ee2b
galvelistious37 <galvelistious37@gmail.com>
2020-03-02 21:37:16 -0800

    Shifting to band builder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 	    // ArrayList<Team> teams;
         // Collections.sort(teams);
@@ -39,9 +41,86 @@ public class Main {
 //
 //        footballLeague.showLeagueTable();
 //        baseballLeague.showLeagueTable();
-        doBandStuff();
+//        doBandStuff();
         // Just adding another line to check pushing stuff
         // Pulled changes pushed from laptop, now pushing from desktop again
+        boolean quit = false;
+        Musician selection;
+
+
+        getGreeting();
+        getInquiry();
+        getMenu();
+
+        while(!quit){
+            if(scanner.hasNextInt()){
+                int enteredValue = scanner.nextInt();
+                scanner.nextLine();
+                if(checkIfQuit(enteredValue)){
+                    quit = true;
+                }
+                if(checkIfBand(enteredValue)){
+                    System.out.println("You chose to start a garage band!");
+                }
+
+                selection = makeSelection(scanner.nextInt());
+
+
+
+            } else {
+                System.out.println("Enter a valid number!");
+            }
+            scanner.nextLine();
+        }
+
+    }
+
+    private static boolean checkIfBand(int enteredValue) {
+        return enteredValue == 1;
+    }
+
+    private static boolean checkIfQuit(int enteredValue) {
+        return enteredValue == 0;
+    }
+
+    private static Musician makeSelection(int nextInt) {
+        String name = "";
+        scanner.nextLine();
+        switch(nextInt){
+            case 2:
+                System.out.println("What is the Guitarist's name?");
+                return new Guitarist(name);
+//            case 3:
+//                System.out.println("Selected to build a Vocalist:");
+//                return 3;
+//            case 4:
+//                System.out.println("Selected to build a Bassist:");
+//                return 4;
+//            case 5:
+//                System.out.println("Selected to build a Drummer:");
+//                return 5;
+//            case 0:
+//                System.out.println("Shutting down ...");
+//                return 0;
+        }
+        return null;
+    }
+
+    private static void getGreeting() {
+        System.out.println("Hello, welcome to build-a-band");
+    }
+
+    private static void getInquiry() {
+        System.out.println("What would you like to do?");
+    }
+
+    private static void getMenu() {
+        System.out.println("1 - Create a Band");
+        System.out.println("2 - Create a Guitarist");
+        System.out.println("3 - Create a Vocalist");
+        System.out.println("4 - Create a Bassist");
+        System.out.println("5 - Create a Drummer");
+        System.out.println("0 - QUit");
     }
 
     private static void doBandStuff() {
